1785855dfbb128ba8557afd0098c809c088fc0a9
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-04-06 10:40:05 -0700

    add business logic for LocationBook and places objects

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function LocationBook() {
+  this.places = [];
+  this.currentId = 0;
+}
+
+LocationBook.prototype.addLocation = function(place) {
+  place.id = this.assignId();
+  this.places.push(place);
+}
+
+LocationBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+// Business for places
+
+function Place(name, landmarks, timeOfYear, notes) {
+  this.name = name;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
